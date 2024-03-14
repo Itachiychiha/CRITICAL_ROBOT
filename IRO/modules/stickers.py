d2d7c82adcaf655634684fa5b97541c90738de47
Itachiuchihaclan <158417056+Itachiychiha@users.noreply.github.com>
2024-03-14 18:17:55 +0530

    Update stickers.py

--- IRO/modules/stickers.py
+++ IRO/modules/stickers.py
@@ -23,8 +23,8 @@ from telegram import (
 from telegram.ext import CallbackContext
 from telegram.utils.helpers import mention_html
 
-from MukeshRobot import dispatcher
-from MukeshRobot.modules.disable import DisableAbleCommandHandler
+from IRO import dispatcher
+from IRO.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
